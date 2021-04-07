c040f4a139fc38cf4bc6ca1ee08dfee59254b42f
bryan18jonah <bryan18jonah@gmail.com>
2021-04-07 12:02:56 +0200

    optimized header banner and added responsive font sizes

--- src/components/HeaderBanner.js
+++ src/components/HeaderBanner.js
@@ -1,12 +1,11 @@
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 export default function HeaderBanner() {
   return (
     <div className='headerbanner-outer-container'>
       <Container className='headerbanner-inner-container'>
-        <h1 className='text-light'><span className='text-primary'>L</span>OREM IPSUM</h1>
-        <h1 className='text-light'>IPSUM DOLOR SIT</h1>
-        <h1 className='text-light'>AMET CONSECTETUR.</h1>
+        <h1 className='text-light'><span className='text-primary'>L</span>OREM IPSUM DOLOR SIT AMET CONSECTETUR.</h1>
+        <Button className='headerbanner-cta float-right'>Sjekk ut vår meny</Button>
       </Container>
     </div>
   )
