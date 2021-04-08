effb3965523f457379bc895fa82622247e53c625
ChristopherTaal <63666767+ChristopherTaal@users.noreply.github.com>
2021-04-08 22:36:16 +0200

    edited heading text

--- src/components/HeaderBanner.js
+++ src/components/HeaderBanner.js
@@ -4,7 +4,8 @@ export default function HeaderBanner() {
   return (
     <div className='headerbanner-outer-container'>
       <Container className='headerbanner-inner-container'>
-        <h1 className='text-light'><span className='text-primary'>L</span>OREM IPSUM DOLOR SIT AMET CONSECTETUR.</h1>
+        <h1 className='text-light'><span className='text-primary'>Å</span>PNER SNART!! </h1>
+        <h2 className='text-light'> Vi gleder oss til å servere dere...</h2>
         <Button className='headerbanner-cta float-right'>Sjekk ut vår meny</Button>
       </Container>
     </div>
